465ca7ddcc76cd04e47e1da45082c5cecdd109fd
d30301469-ui <d30301469@gmail.com>
2026-06-29 04:00:26 +0400

    update

--- pages/beni-ara.php
+++ pages/beni-ara.php
@@ -1,6 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+    require_once __DIR__ . '/../config/frontend_session.php';
+    metropol_frontend_session_start();
 }
 require_once __DIR__ . '/../views/layouts/head_full.php';
 include __DIR__ . '/../views/partials/header.php';
