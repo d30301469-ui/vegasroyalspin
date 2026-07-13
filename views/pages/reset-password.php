b65e4787a65249cda4fd3e5e712f79f4e3486f9c
d30301469-ui <d30301469@gmail.com>
2026-07-14 02:08:34 +0400

    Show reset-password as standalone modal-only page

--- views/pages/reset-password.php
+++ views/pages/reset-password.php
@@ -2,7 +2,5 @@
 $resetToken = $resetToken ?? '';
 ?>
 <?php include VIEW_PATH . '/layouts/head.php'; ?>
-<?php include VIEW_PATH . '/partials/header.php'; ?>
 <?php include VIEW_PATH . '/partials/reset-password-section.php'; ?>
-<?php include VIEW_PATH . '/partials/footer.php'; ?>
 <script src="/assets/js/reset-password.js" defer></script>
