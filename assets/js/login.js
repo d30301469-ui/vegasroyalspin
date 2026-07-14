a44a583c5f2e34f064b825eb1264fd50b7a111ae
d30301469-ui <d30301469@gmail.com>
2026-07-15 02:29:33 +0400

    Set fixed mobile forgot top offset to move form lower

--- assets/js/login.js
+++ assets/js/login.js
@@ -274,13 +274,13 @@
             if (isMobileSite) {
                 if (forg) {
                     forg.style.justifyContent = 'flex-start';
-                    forg.style.paddingTop = 'clamp(340px, 48vh, 430px)';
+                    forg.style.paddingTop = '390px';
                     forg.style.paddingBottom = '12px';
                     forg.style.width = 'calc(100% - 24px)';
                     forg.style.margin = '0 12px';
                 }
                 forgotForm.style.flex = '0 0 auto';
-                forgotForm.style.marginTop = '16px';
+                forgotForm.style.marginTop = '18px';
                 forgotForm.style.justifyContent = '';
             } else {
                 forgotForm.style.flex = '0 0 auto';
