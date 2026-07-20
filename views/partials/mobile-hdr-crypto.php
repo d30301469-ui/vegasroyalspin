7fbbaef61c10b23a923b4f99e4b273b88a4dadff
d30301469-ui <d30301469@gmail.com>
2026-07-20 22:42:43 +0400

    Fix mobile wallet shortcut panel

--- views/partials/mobile-hdr-crypto.php
+++ views/partials/mobile-hdr-crypto.php
@@ -7,7 +7,7 @@ $loggedIn = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true;
           class="btn a-color connect-wallet"
           id="connectWalletBtn"
           title="Cüzdana Bağlan"
-          onclick="<?php if ($loggedIn): ?>if (typeof redirectToDeposit === 'function') redirectToDeposit();<?php else: ?>if (typeof showLoginWarning === 'function') showLoginWarning();<?php endif; ?>">
+          onclick="<?php if ($loggedIn): ?>if (typeof window.__openMobileBalancePage === 'function' && window.__openMobileBalancePage('deposit')) return false; if (typeof redirectToDeposit === 'function') redirectToDeposit(); return false;<?php else: ?>if (typeof showLoginWarning === 'function') showLoginWarning();<?php endif; ?>">
     <span>CÜZDANA BAĞLAN</span>
   </button>
 </div>
