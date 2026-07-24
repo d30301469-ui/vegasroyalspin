6a3aedd0c0ca02c95d8079f949d3d07daacd7a23
d30301469-ui <d30301469@gmail.com>
2026-07-24 15:07:11 +0400

    Harden mobile/PWA menu open and scroll unlock

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* vegasroyalspin PWA service worker */
-const SW_VERSION = 'v8-mobile-menu-scroll-lock-fix';
+const SW_VERSION = 'v9-mobile-menu-delegation-lock-recovery';
 const STATIC_CACHE = `vrs-static-${SW_VERSION}`;
 
 const PRE_CACHE_URLS = [
